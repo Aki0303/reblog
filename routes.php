bb09f281fa8b427a41f55c5a7287ecfb101f84a5
Atsuki Wada <gnawedapple.macapple@gmail.com>
2016-09-29 13:30:40 +0800

    routes.phpの作成

--- routes.php
+++ routes.php
@@ -1,5 +1,23 @@
 <?php
-  echo 'rootes.phpを通りました。';
+  echo 'rootes.phpを通りました。<br>';
+
+
+  // １、GETパラメータを取得
+  // explode関数：第二引数の文字列を、第一引数の文字列で分解し、配列で返す関数。
+  $params = explode("/", $_GET['url']);
+
+  // ２，パラメータの分解
+  $resouce = $params[0];
+  $action  = $params[1];
+    // $idはオプションを取得するものであるが、オプションは存在するかわからないため、$id = 0;
+  $id = 0;
+  if (isset($params[2])) {
+    $id = $params[2];
+  }
+
+  // ３、コントローラの呼び出し
+  require('controllers/'. $resouce .'_controller.php');
+
 
 
 ?>
